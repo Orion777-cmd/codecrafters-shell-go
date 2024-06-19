bec8adc0f4fe7aea142705c9385bc5923ff29243
orion777-cmd <abiy.biru78@gmail.com>
2024-06-19 10:53:24 +0300

    pass 3rd stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,11 +7,14 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-	output, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		output, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			return
+		}
+		fmt.Printf("%v: command not found\n", output[:len(output)-1])
+
 	}
-	fmt.Printf("%v: command not found\n", output[:len(output)-1])
 }
