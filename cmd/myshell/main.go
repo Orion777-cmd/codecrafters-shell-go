f090b7630f2c94a1ab1afa8371c7bdd70a7fb2d6
orion777-cmd <abiy.biru78@gmail.com>
2024-06-19 11:18:12 +0300

    pass 6th stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,6 +8,14 @@ import (
 )
 
 func main() {
+	builtins := make(map[string]bool)
+	builtins["echo"] = true
+	builtins["exit"] = true
+	builtins["type"] = true
+	builtins["cd"] = true
+	builtins["pwd"] = true
+
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		output, err := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -25,7 +33,14 @@ func main() {
 				return
 			case "echo":
 				fmt.Printf("%v\n", strings.Join(splitOutput[1:], " "))
+			case "type":
+				if builtins[splitOutput[1]] {
+					fmt.Printf("%v is a shell builtin\n", splitOutput[1])
+				} else {
+					fmt.Printf("%v: not found\n", splitOutput[1])
+				}
 			default:
+				
 				fmt.Printf("%v: command not found\n", output[:len(output)-1])
 		}
 	}
