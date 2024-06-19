8f6bf94b6dcf02091f34c85750bdae3230f85ff8
orion777-cmd <abiy.biru78@gmail.com>
2024-06-19 11:31:51 +0300

    pass 7th stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -14,7 +15,6 @@ func main() {
 	builtins["type"] = true
 	builtins["cd"] = true
 	builtins["pwd"] = true
-
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -29,19 +29,34 @@ func main() {
 		}
 		command := splitOutput[0]
 		switch command {
-			case "exit":
-				return
-			case "echo":
-				fmt.Printf("%v\n", strings.Join(splitOutput[1:], " "))
-			case "type":
-				if builtins[splitOutput[1]] {
-					fmt.Printf("%v is a shell builtin\n", splitOutput[1])
-				} else {
+		case "exit":
+			return
+		case "echo":
+			fmt.Printf("%v\n", strings.Join(splitOutput[1:], " "))
+		case "type":
+			if builtins[splitOutput[1]] {
+				fmt.Printf("%v is a shell builtin\n", splitOutput[1])
+			} else {
+				pathVar := os.Getenv("PATH")
+				paths := strings.Split(pathVar, ":")
+				found := false
+				for _, path := range paths {
+					fullPath := filepath.Join(path, splitOutput[1])
+					info, err := os.Stat(fullPath)
+					if err == nil && info.Mode().IsRegular() && info.Mode().Perm()&0111 != 0 {
+						fmt.Printf("%v is %v\n", splitOutput[1], fullPath)
+						found = true
+						break
+					}
+				}
+				if !found {
+
 					fmt.Printf("%v: not found\n", splitOutput[1])
 				}
-			default:
-				
-				fmt.Printf("%v: command not found\n", output[:len(output)-1])
+			}
+		default:
+
+			fmt.Printf("%v: command not found\n", output[:len(output)-1])
 		}
 	}
 }
